9aa35d834d07a127d70cfbc845d9da2834f05f97
Ivan <i_am_vanya@mail.ru>
2020-05-10 21:23:56 +0300

    models v.2

--- app/main_controller.py
+++ app/main_controller.py
@@ -13,26 +13,6 @@ main = Blueprint('main', __name__, template_folder='app/templates', static_folde
 logger = logging.getLogger(__name__)
 
 
-# @main.route('/')
-# def index():
-#     py = Visitor(token=fake.name(), username='Visitor')
-#     logger.info(str(py))
-#     Comment(comment_text='comment1', visitor=py)
-#     db.session.add(py)
-#     db.session.commit()
-#     try:
-#         visitor = db.session.query(Visitor).filter_by(username="Visitor").first()
-#         logger.info(str(visitor))
-#         # p = Comment(comment_text='second')
-#         # visitor.comments.append(p)
-#         Comment(comment_text='comment2', visitor=visitor)
-#     except Exception as ex:
-#         logger.warning(str(ex))
-#
-#     search = db.session.query(Visitor).filter_by(username="Visitor").first()
-#     return str(Comment.query.with_parent(search).all())
-
-
 @main.route('/')
 def index():
     services.add_visitor(fake.name(), 'Visitor')
