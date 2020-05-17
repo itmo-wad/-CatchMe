b2f8e163fb0a6662acdbaad5a2315bed4b3dbecf
mjumnir <mjumnir@gmail.com>
2020-05-17 23:45:43 +0000

    First container

--- src/init_DB_ TEMP.sql
+++ src/init_DB_ TEMP.sql
@@ -3,5 +3,5 @@
 DROP DATABASE IF EXISTS CommentCloud;
 DROP USER IF EXISTS simple_user;
 
-CREATE USER simple_user WITH LOGIN PASSWORD 'simple_user';
+CREATE USER simple_user WITH LOGIN ENCRYPTED PASSWORD 'simple_user';
 CREATE DATABASE CommentCloud;
