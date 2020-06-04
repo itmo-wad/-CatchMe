cda6094d49f1c83d38735d50677c5e5c8ea2b0a4
mjumnir <mjumnir@gmail.com>
2020-06-04 15:47:42 +0300

    Addin secrets.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -6,6 +6,8 @@ from flask_login import LoginManager
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from . import secrets as s
+
 db = SQLAlchemy()
 secret_key = os.urandom(16)
 
@@ -30,8 +32,8 @@ def register_blueprints(app):
 
 
 def create_db(app):
-    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user='simple_user', pw='simple_user', url='db:5432',
-                                                                   db='commentcloud')
+    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=s.user, pw=s.pw, url=s.url,
+                                                                   db=s.db)
     app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = True
--- src/init_DB_ TEMP.sql
+++
@@ -1,7 +0,0 @@
-
-\c postgres
-DROP DATABASE IF EXISTS CommentCloud;
-DROP USER IF EXISTS simple_user;
-
-CREATE USER simple_user WITH LOGIN ENCRYPTED PASSWORD 'simple_user';
-CREATE DATABASE CommentCloud;
